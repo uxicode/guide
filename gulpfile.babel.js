5a965937b8175ca9bcf398da39e74ff9bacede33
전봉철 <jeonbongcheol@IKOOBui-MacBookPro.local>
2023-03-07 18:36:35 +0900

    메뉴수정

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -110,7 +110,8 @@ const JS_CODE=`<script>
             let scrollIdx = $(this).attr('href');
              let offsetY=$(scrollIdx).offset().top;
              $('html, body').stop().animate({scrollTop:offsetY}, 350 );
-             
+             $('.side-bar').toggleClass('active');
+             $('.m-bar').toggleClass('active');
             activeMenus( parseInt( $(this).attr('data-index')) );
         });
         scrollSpyBtnItems.on('mouseenter', function(e){
